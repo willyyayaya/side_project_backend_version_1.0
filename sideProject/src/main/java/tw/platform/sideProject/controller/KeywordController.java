6d679444711856a1e66915f9b86aae746c1adccc
Jessie-YuWen <jk1765688@gmail.com>
2025-01-13 16:34:16 +0800

    1/13 關鍵字功能完成

--- sideProject/src/main/java/tw/platform/sideProject/controller/KeywordController.java
+++ sideProject/src/main/java/tw/platform/sideProject/controller/KeywordController.java
@@ -3,6 +3,7 @@ package tw.platform.sideProject.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,22 +14,19 @@ import tw.platform.sideProject.service.KeywordService;
 
 @Controller
 public class KeywordController {
-	
+
 	@Autowired
 	private KeywordService keywordService;
-	
+
 	@PostMapping("/saveKeyword")
-	public ResponseEntity<String> saveKeyword(@RequestParam String userKeyword){
+	public ResponseEntity<String> saveKeyword(@RequestParam String userKeyword) {
 		keywordService.saveKeyword(userKeyword);
 		return ResponseEntity.ok("新增成功");
 	}
-	
-//	public void hotWord() {
-//		List<Keywords> list = keywordService.getKeywordDesc();
-//		for(Keywords keywords : list) {
-//			System.out.println(keywords.getKeyword());
-//		}
-//		
-//	}
 
+	@GetMapping("/randomKeyword")
+	public ResponseEntity<String> getRandomKeyword() {
+		String randomKeyword = keywordService.getRandomKeyword();
+		return new ResponseEntity<>(randomKeyword, HttpStatus.OK);
+	}
 }
--- sideProject/src/main/java/tw/platform/sideProject/service/KeywordService.java
+++ sideProject/src/main/java/tw/platform/sideProject/service/KeywordService.java
@@ -12,28 +12,33 @@ import tw.platform.sideProject.repository.KeywordRepository;
 
 @Service
 public class KeywordService {
-	
+
 	@Autowired
 	private KeywordRepository keywordRepository;
-	
+
 	public void saveKeyword(String userKeyword) {
 		Optional<Keywords> SQLKeyword = keywordRepository.findByKeyword(userKeyword);
-		if(SQLKeyword.isPresent()) {
+		if (SQLKeyword.isPresent()) {
 			Keywords keyword = SQLKeyword.get();
-			keyword.setTimes(keyword.getTimes()+1);
+			keyword.setTimes(keyword.getTimes() + 1);
 			keywordRepository.save(keyword);
-			System.out.println("關鍵詞 "+keyword.getKeyword()+" 搜尋次數+1");
-		}else {
+			System.out.println("關鍵詞 " + keyword.getKeyword() + " 搜尋次數+1");
+		} else {
 			Keywords newKeyword = new Keywords();
 			newKeyword.setKeyword(userKeyword);
 			newKeyword.setTimes(1);
 			keywordRepository.save(newKeyword);
-			System.out.println("新增 "+newKeyword.getKeyword()+" 關鍵詞");
+			System.out.println("新增 " + newKeyword.getKeyword() + " 關鍵詞");
 		}
 	}
-	
-	public List<Keywords> getKeywordDesc(){
+
+	public List<Keywords> getKeywordDesc() {
 		List<Keywords> top5Keywords = keywordRepository.findTop5ByOrderByTimesDesc();
 		return top5Keywords;
+	}
+
+	public String getRandomKeyword() {
+		Keywords keywords = keywordRepository.findRandomKeyword();
+		return keywords != null ? keywords.getKeyword() : "目前無其他關鍵字";
 	}
 }
