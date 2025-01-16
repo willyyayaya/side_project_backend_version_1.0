3b1b66e645db2fce595bb2d60c2449f12c4d570a
Jessie-YuWen <jk1765688@gmail.com>
2025-01-16 10:34:39 +0800

    1/16 register寬度調整+mimiJava Table對應

--- sideProject/src/main/java/tw/platform/sideProject/model/mimiMember.java
+++ sideProject/src/main/java/tw/platform/sideProject/model/mimiMember.java
@@ -13,9 +13,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 
 @Entity
+@Table(name = "member")
 public class mimiMember {
 	
 	@Id
--- sideProject/src/main/java/tw/platform/sideProject/model/mimiTag.java
+++ sideProject/src/main/java/tw/platform/sideProject/model/mimiTag.java
@@ -7,8 +7,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tag")
 public class mimiTag {
 	
 	@Id
