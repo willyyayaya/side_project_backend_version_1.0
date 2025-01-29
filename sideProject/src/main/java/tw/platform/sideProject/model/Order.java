9d1b02499a16f45d7fc342f638617e520e696ba8
willyyayaya <willyyayaya@gmail.com>
2025-01-29 18:24:27 +0800

    1/29 除錯預設大頭照功能(尚未完成)

--- sideProject/src/main/java/tw/platform/sideProject/model/Order.java
+++ sideProject/src/main/java/tw/platform/sideProject/model/Order.java
@@ -30,7 +30,7 @@ public class Order {
     private String deadline;
     private String intro;
     private String detail;
-    private byte[] picurl;
+    private String picurl;
     private String location;
     private Integer rank;
     private Integer people = 1; // 預設值
@@ -94,11 +94,11 @@ public class Order {
         this.detail = detail;
     }
 
-    public byte[] getPicurl() {
+    public String getPicurl() {
         return picurl;
     }
 
-    public void setPicurl(byte[] picurl) {
+    public void setPicurl(String picurl) {
         this.picurl = picurl;
     }
 
