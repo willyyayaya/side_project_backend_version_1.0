7cd4f60b6e9f3f6d21dc538e825e10e9944899ec
baobao <er905322@gmail.com>
2025-02-05 13:24:43 +0800

    0205 舊的mo

--- sideProject/src/main/java/tw/platform/sideProject/model/MemberOrder.java
+++ sideProject/src/main/java/tw/platform/sideProject/model/MemberOrder.java
@@ -1,7 +1,9 @@
-package tw.platform.sideProject.model;
-
+package tw.sideproject.model;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
@@ -12,6 +14,7 @@ import jakarta.persistence.Table;
 public class MemberOrder {
 
     @EmbeddedId
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private MemberOrderKey id;
 
     @ManyToOne
@@ -26,19 +29,62 @@ public class MemberOrder {
 
     private Boolean owned;
     private Boolean wanted;
-    private boolean collected;
+    private Boolean collected;
+    private Boolean getproject;
     private String evaluate;
-    private boolean getproject;
     private String picurl;   // 確保有這個屬性
     private String name;
+    private String detail;
+    
 
 
     // Getters and Setters
+    
+    public Long getMemberid() {
+        return id.getMemberid();
+    }
+
+    public Long getOrderid() {
+        return id.getOrderid();
+    }
+
+    
     public MemberOrderKey getId() {
         return id;
     }
 
-    public void setId(MemberOrderKey id) {
+
+	public String getPicurl() {
+		return picurl;
+	}
+
+
+	public void setPicurl(String picurl) {
+		this.picurl = picurl;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDetail() {
+		return detail;
+	}
+
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+
+	public void setId(MemberOrderKey id) {
         this.id = id;
     }
 
@@ -74,6 +120,33 @@ public class MemberOrder {
         this.wanted = wanted;
     }
 
+	public Boolean isCollected() {
+		return collected;
+	}
+
+
+	public void setCollected(Boolean collected) {
+		this.collected = collected;
+	}
+    
+	@Override
+	public String toString() {
+	    return "MemberOrder{" +
+	            "orderid=" + getOrderid() +
+	            ", name='" + name + '\'' +
+	            ", picurl='" + picurl + '\'' +
+	            ", collected=" + collected +
+	            '}';
+	}
+
+	public Boolean getGetproject() {
+		return getproject;
+	}
+
+	public void setGetproject(Boolean getproject) {
+		this.getproject = getproject;
+	}
+
 	public String getEvaluate() {
 		return evaluate;
 	}
@@ -82,30 +155,6 @@ public class MemberOrder {
 		this.evaluate = evaluate;
 	}
 
-	public boolean isCollected() {
-		return collected;
-	}
-
-	public void setCollected(boolean collected) {
-		this.collected = collected;
-	}
-
-	public boolean isGetproject() {
-		return getproject;
-	}
-
-	public void setGetproject(boolean getproject) {
-		this.getproject = getproject;
-	}
 	
-	@Override
-	public String toString() {
-	    return "MemberOrder{" +
-	            "orderid=" + getOrder() +
-	            ", name='" + name + '\'' +
-	            ", picurl='" + picurl + '\'' +
-	            ", collected=" + collected +
-	            '}';
-	}
 	
 }
